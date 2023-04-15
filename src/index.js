324d463973ad38e79615e220d583678607cb672a
SHAGAR Thangaraj <thangaraj.shagar@wfp.org>
2023-04-15 07:50:21 +0530

    changedNewOLD

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import {BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-root.render(<BrowserRouter><App/></BrowserRouter>);
+const root =  ReactDOM.createRoot(document.getElementById('root'))
+root.render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
